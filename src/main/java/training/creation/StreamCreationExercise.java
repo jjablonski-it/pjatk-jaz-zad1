e6b1c2c99ab53e41d27cb96ed55ea5a104e598b1
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-11-18 20:52:36 +0100

    creation done

--- src/main/java/training/creation/StreamCreationExercise.java
+++ src/main/java/training/creation/StreamCreationExercise.java
@@ -1,5 +1,6 @@
 package training.creation;
 
+import java.math.BigDecimal;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -8,32 +9,33 @@ public class StreamCreationExercise implements StreamCreator {
 
     @Override
     public IntStream positiveDigitsUsingOf() {
-        return IntStream.empty();
+        return IntStream.of(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).filter(val -> val > 0);
     }
 
     @Override
     public IntStream positiveDigitsUsingRange() {
-        return IntStream.empty();
+        return IntStream.range(-9, 10).filter(val -> val > 0);
     }
 
     @Override
     public IntStream positiveDigitsUsingIterate() {
-        return IntStream.empty();
+        return IntStream.iterate(-9, val -> val + 1).limit(19).filter(val -> val > 0);
     }
 
     @Override
     public IntStream powersOfTwo() {
-        return IntStream.empty();
+        return IntStream.range(1, 11).map(pow -> (int) Math.pow(2, pow));
     }
 
     @Override
     public DoubleStream from0Till1WithTenSteps() {
-        return DoubleStream.empty();
+        return DoubleStream.iterate(0, val -> val + 0.1).limit(10).map(val -> (double) Math.round(val * 10) / 10);
     }
 
     @Override
     public Stream<String> alphabet() {
-        return Stream.empty();
+        IntStream chars = IntStream.range(65, 91);
+        return chars.mapToObj(val -> Character.toString(val));
     }
 
 }
