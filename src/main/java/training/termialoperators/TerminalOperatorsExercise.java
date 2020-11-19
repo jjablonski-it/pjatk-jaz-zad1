936d4110dfc26e42010d224f9e22b29c8017c580
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-11-19 01:05:12 +0100

    terminal done

--- src/main/java/training/termialoperators/TerminalOperatorsExercise.java
+++ src/main/java/training/termialoperators/TerminalOperatorsExercise.java
@@ -1,57 +1,60 @@
 package training.termialoperators;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TerminalOperatorsExercise implements StreamTerminalOperators {
 
     @Override
     public void addToSet(Stream<String> stream, Set<String> set) {
+        stream.forEach(set::add);
     }
 
     @Override
     public void addToListInReverseOrder(Stream<String> stream, List<String> list) {
+        stream.sorted(Comparator.reverseOrder()).forEach(list::add);
     }
 
     @Override
     public String[] collectToArray(Stream<String> stream) {
-        return new String[0];
+        return stream.toArray(String[]::new);
     }
 
     @Override
     public Set<String> collectToSet(Stream<String> stream) {
-        return Set.of();
+        return stream.collect(Collectors.toSet());
     }
 
     @Override
     public List<String> collectToList(Stream<String> stream) {
-        return List.of();
+        return stream.collect(Collectors.toList());
     }
 
     @Override
     public String mergeIntoString(Stream<String> stream) {
-        return "";
+        return stream.collect(Collectors.joining("-", "[", "]"));
     }
 
     @Override
     public int countCharacters(Stream<String> stream) {
-        return 0;
+        return stream.mapToInt(String::length).sum();
     }
 
     @Override
     public int longestWordLength(Stream<String> stream) {
-        return 0;
+        return stream.mapToInt(String::length).reduce(0, (max, len) -> len > max ? len : max);
     }
 
     @Override
     public String longestWord(Stream<String> stream) {
-        return "";
+        return stream.sorted((s1, s2) -> s2.length() - s1.length()).limit(1).collect(Collectors.joining());
     }
 
     @Override
     public Map<Integer, List<String>> wordsGroupedByLength(Stream<String> stream) {
-        return Map.of();
+        return stream.collect(Collectors.groupingBy(String::length));
     }
 }
